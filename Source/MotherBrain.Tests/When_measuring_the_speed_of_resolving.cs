83428ec4d18f7857cef87e1b1d12458b329feea0
rmacfie <robert@macfie.se>
2013-01-14 23:45:02 +0100

    Added speed test

---
+++ Source/MotherBrain.Tests/When_measuring_the_speed_of_resolving.cs
@@ -0,0 +1,36 @@
+﻿namespace MotherBrain.Tests
+{
+    using System.Diagnostics;
+    using Machine.Specifications;
+
+    public class When_speedtesting_simple_lambda_registrations
+    {
+        const int iterations = 100000;
+        const int maximumTimeMs = 100;
+
+        static Stopwatch stopwatch;
+        static IContainer container;
+
+        Establish context = () =>
+        {
+            stopwatch = new Stopwatch();
+            container = new Container();
+            container.Register<AService, IService>(c => new AService());
+        };
+
+        Because of = () =>
+        {
+            stopwatch.Start();
+            for (var i = 0; i < iterations; i++)
+            {
+                var instance = container.Resolve<IService>();
+            }
+            stopwatch.Stop();
+
+            Debug.WriteLine("Built {0} instances in {1} ms.", iterations, stopwatch.ElapsedMilliseconds);
+        };
+
+        It should_be_quick = () =>
+            stopwatch.ElapsedMilliseconds.ShouldBeLessThan(maximumTimeMs);
+    }
+}
