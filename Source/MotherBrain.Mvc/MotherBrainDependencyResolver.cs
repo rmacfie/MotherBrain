b4d2992e99363dbce560841d94ded7216d61185c
rmacfie <robert@macfie.se>
2013-01-16 09:04:21 +0100

    Adding Mvc adapter (work in progress)

---
+++ Source/MotherBrain.Mvc/MotherBrainDependencyResolver.cs
@@ -0,0 +1,26 @@
+﻿namespace MotherBrain.Mvc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    public class MotherBrainDependencyResolver : IDependencyResolver
+    {
+        IContainer container;
+
+        public MotherBrainDependencyResolver(IContainer container)
+        {
+            this.container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/MotherBrain.Mvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MotherBrain.Mvc")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("b1451a04-eab0-46cf-a63e-21553f260fb0")]
